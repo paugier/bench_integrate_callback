8e0a13675a5d78b28468e585bcc704a90b213c1a
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-01-06 04:44:40 +0100

    Fix simple_quad.py

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Integration and ODEs (scipy.integrate)
 
 https://docs.scipy.org/doc/scipy/reference/integrate.html
 
+https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html#faster-integration-using-low-level-callback-functions
+
 https://github.com/JuliaDiffEq/SciPyDiffEq.jl
--- simple_quad.py
+++ simple_quad.py
@@ -13,23 +13,31 @@ from _pythran import integrand as integrand_capsule
 def integrand(x):
     return 10.0 * x * np.cos(x)
 
+
+integrand_transonic_jit = jit(native=True)(integrand)
+
+# transonic bug (should work)!
+# integrand_transonic_boost = boost(integrand)
+
+
 @boost
-def integrand_transonic_boost(x):
+def integrand_transonic_boost(x: float):
     return 10.0 * x * np.cos(x)
 
 
-integrand_transonic_jit = jit(native=True)(integrand)
-# integrand_transonic_boost = boost(native=True)(integrand)
 integrand_numba = numba.njit(integrand)
+integrand_numba_cfunc = numba.cfunc("float64(float64)")(integrand)
 
-callable = LowLevelCallable(integrand_capsule, signature="double (double)")
+
+ll_callable = LowLevelCallable(integrand_capsule, signature="double (double)")
 
 methods = {
     "no acceleration": "integrand",
     "numba": "integrand_numba",
+    "numba cfunc": "integrand_numba_cfunc.ctypes",
     "transonic_jit": "integrand_transonic_jit",
     "transonic_boost": "integrand_transonic_boost",
-    "pythran capsule": "callable",
+    "pythran capsule": "ll_callable",
 }
 norm = None
 for name, key in methods.items():
